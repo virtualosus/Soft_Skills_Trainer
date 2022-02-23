4ee096628b896e77f8eab487d720c19d0f5e9d0f
2030428 <72548933+2030428@users.noreply.github.com>
2022-02-23 16:23:27 +0000

    got pulling dialogue back out of Yarn into list dependant on character working

--- Assets/Scripts/TTS Scripts/LineManager.cs
+++ Assets/Scripts/TTS Scripts/LineManager.cs
@@ -3,87 +3,141 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 using Yarn.Markup;
+using UnityEngine.UI;
+using System.Linq;
 
 public class LineManager : MonoBehaviour
 {
+    public SpeechManager char1SpeechManager, char2SpeechManager;
 
-    public SpeechManager BoySpeechManager, GirlSpeechManager;
-    //public LineView LineView;
-    public string lineToBeSpoken;
-    public MarkupParseResult MarkupParseResult;
+    public YarnProject myYarnProject;
 
+    private List<string> allLinesIDList = new List<string>();
+    private List<string> char1LineIDList = new List<string>();
+    private List<string> char2LineIDList = new List<string>();
+    public List<string> char1TextLineList = new List<string>();
+    public List<string> char2TextLineList = new List<string>();
 
+    //public string[] char1LineArray, char2LineArray;
 
+    //public string lineIDsString, lineTextString;
+    
+    //public Text debugText;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        GetLineIDs();
+        SortLineIDs();
+        GetLinesFromIDs();
     }
 
-    // Update is called once per frame
-    void Update()
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //}
+
+    public void GetLineIDs()
     {
-        UpdateLine();
+        var lineID = myYarnProject.GetLocalization("En").GetLineIDs();
+        //Debug.LogError(lineID);
+        allLinesIDList = lineID.ToList();
     }
 
-    public void UpdateLine()
+
+    public void SortLineIDs()
     {
-        lineToBeSpoken = MarkupParseResult.Text;
-        //Debug.LogError("updating line...");
-    }
-
-
-
-
-    public void BoySpeechPlayback()
-    {
-        if (BoySpeechManager.isReady)
+        foreach (string tempString in allLinesIDList)
         {
-
-            //lineToBeSpoken = MarkupParseResult.Text;
-
-            //lineToBeSpoken = 
-                
-
-
-
-
-
-            //lineToBeSpoken = LineView.sendline
-            //SpeechManager.voiceName = (VoiceName)voicelist.value;
-            //SpeechManager.VoicePitch = int.Parse(pitch.text);
-
-
-            // Required to insure non-blocking code in the main Unity UI thread.
-            BoySpeechManager.SpeakWithSDKPlugin(lineToBeSpoken);
-            
-
-        }
-        else
-        {
-            Debug.Log("SpeechManager is not ready. Wait until authentication has completed.");
+            if (tempString.Contains("line:char1"))
+            {
+                char1LineIDList.Add(tempString);
+            }
+            if (tempString.Contains("line:char2"))
+            {
+                char2LineIDList.Add(tempString);
+            }
         }
     }
 
-    public void GirlSpeechPlayback()
+    public void GetLinesFromIDs()
     {
-        if (GirlSpeechManager.isReady)
+        foreach (string tempString in char1LineIDList)
         {
-            string msg = lineToBeSpoken;
-            //SpeechManager.voiceName = (VoiceName)voicelist.value;
-            //SpeechManager.VoicePitch = int.Parse(pitch.text);
-
-
-            // Required to insure non-blocking code in the main Unity UI thread.
-            GirlSpeechManager.SpeakWithSDKPlugin(msg);
-
-
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1TextLineList.Add(text);
         }
-        else
+
+        foreach (string tempString in char2LineIDList)
         {
-            Debug.Log("SpeechManager is not ready. Wait until authentication has completed.");
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2TextLineList.Add(text);
         }
+
+        //var text = myYarnProject.GetLocalization("En").GetLocalizedString("line:01c1039");
+        //lineTextString = text;
+        //Debug.LogError(text);
     }
+
+
+
+
+
+
+
+    //public void BoySpeechPlayback()
+    //{
+    //    if (BoySpeechManager.isReady)
+    //    {
+
+    //        //lineToBeSpoken = MarkupParseResult.Text;
+
+    //        //lineToBeSpoken = 
+
+
+
+
+
+
+    //        //lineToBeSpoken = LineView.sendline
+    //        //SpeechManager.voiceName = (VoiceName)voicelist.value;
+    //        //SpeechManager.VoicePitch = int.Parse(pitch.text);
+
+
+    //        // Required to insure non-blocking code in the main Unity UI thread.
+    //        //BoySpeechManager.SpeakWithSDKPlugin(lineToBeSpoken);
+
+
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("SpeechManager is not ready. Wait until authentication has completed.");
+    //    }
+    //}
+
+    //public void GirlSpeechPlayback()
+    //{
+    //    if (GirlSpeechManager.isReady)
+    //    {
+    //        //string msg = lineToBeSpoken;
+    //        //SpeechManager.voiceName = (VoiceName)voicelist.value;
+    //        //SpeechManager.VoicePitch = int.Parse(pitch.text);
+
+
+    //        // Required to insure non-blocking code in the main Unity UI thread.
+    //        //GirlSpeechManager.SpeakWithSDKPlugin(msg);
+
+
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("SpeechManager is not ready. Wait until authentication has completed.");
+    //    }
+    //}
 
 }
