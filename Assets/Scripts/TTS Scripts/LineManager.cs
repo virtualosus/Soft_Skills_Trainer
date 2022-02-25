b95f6ea2c53c78201bafd1c094fed42868b11c69
2030428 <72548933+2030428@users.noreply.github.com>
2022-02-25 11:32:36 +0000

    new yarn to accomodate branching dialogue - updating line manager

--- Assets/Scripts/TTS Scripts/LineManager.cs
+++ Assets/Scripts/TTS Scripts/LineManager.cs
@@ -8,21 +8,82 @@ using System.Linq;
 
 public class LineManager : MonoBehaviour
 {
+
     public YarnProject myYarnProject;
 
     public SpeechManager char1SpeechManager, char2SpeechManager;
     public ClassroomSpeechToYarn ClassroomTestSpeechToYarn;
     public LineView LineView;
+    public AudioSource Char1AudioSource, Char2AudioSource;
 
     private List<string> allLinesIDList = new List<string>();
     private List<string> char1LineIDList = new List<string>();
     private List<string> char2LineIDList = new List<string>();
+    private List<string> char1NegativeLineIDList = new List<string>();
+    private List<string> char2NegativeLineIDList = new List<string>();
+    private List<string> char1NeutralLineIDList = new List<string>();
+    private List<string> char2NeutralLineIDList = new List<string>();
+    private List<string> char1PositiveLineIDList = new List<string>();
+    private List<string> char2PositiveLineIDList = new List<string>();
+
+    [Header("Stage 1 Lines")]
+    private List<string> char1NegativeLineStage1IDList = new List<string>();
+    private List<string> char2NegativeLineStage1IDList = new List<string>();
+    private List<string> char1NeutralLineStage1IDList = new List<string>();
+    private List<string> char2NeutralLineStage1IDList = new List<string>();
+    private List<string> char1PositiveLineStage1IDList = new List<string>();
+    private List<string> char2PositiveLineStage1IDList = new List<string>();
+
+    private List<string> char1NegativeLineStage2IDList = new List<string>();
+    private List<string> char2NegativeLineStage2IDList = new List<string>();
+    private List<string> char1NeutralLineStage2IDList = new List<string>();
+    private List<string> char2NeutralLineStage2IDList = new List<string>();
+    private List<string> char1PositiveLineStage2IDList = new List<string>();
+    private List<string> char2PositiveLineStage2IDList = new List<string>();
+
+    private List<string> char1NegativeLineStage3IDList = new List<string>();
+    private List<string> char2NegativeLineStage3IDList = new List<string>();
+    private List<string> char1NeutralLineStage3IDList = new List<string>();
+    private List<string> char2NeutralLineStage3IDList = new List<string>();
+    private List<string> char1PositiveLineStage3IDList = new List<string>();
+    private List<string> char2PositiveLineStage3IDList = new List<string>();
+
+    [Header("All Lines")]
     public List<string> char1TextLineList = new List<string>();
     public List<string> char2TextLineList = new List<string>();
+    public List<string> char1NegativeLineList = new List<string>();
+    public List<string> char2NegativeLineList = new List<string>();
+    public List<string> char1NeutralLineList = new List<string>();
+    public List<string> char2NeutralLineList = new List<string>();
+    public List<string> char1PositiveLineList = new List<string>();
+    public List<string> char2PositiveLineList = new List<string>();
+
+    [Header("Stage 1 Lines")]
+    public List<string> char1NegativeLineStage1List = new List<string>();
+    public List<string> char2NegativeLineStage1List = new List<string>();
+    public List<string> char1NeutralLineStage1List = new List<string>();
+    public List<string> char2NeutralLineStage1List = new List<string>();
+    public List<string> char1PositiveLineStage1List = new List<string>();
+    public List<string> char2PositiveLineStage1List = new List<string>();
+    
+    [Header("Stage 2 Lines")]
+    public List<string> char1NegativeLineStage2List = new List<string>();
+    public List<string> char2NegativeLineStage2List = new List<string>();
+    public List<string> char1NeutralLineStage2List = new List<string>();
+    public List<string> char2NeutralLineStage2List = new List<string>();
+    public List<string> char1PositiveLineStage2List = new List<string>();
+    public List<string> char2PositiveLineStage2List = new List<string>();
+
+    [Header("Stage 3 Lines")]
+    public List<string> char1NegativeLineStage3List = new List<string>();
+    public List<string> char2NegativeLineStage3List = new List<string>();
+    public List<string> char1NeutralLineStage3List = new List<string>();
+    public List<string> char2NeutralLineStage3List = new List<string>();
+    public List<string> char1PositiveLineStage3List = new List<string>();
+    public List<string> char2PositiveLineStage3List = new List<string>();
+
 
     public int char1LineCount, char2LineCount;
-
-    public AudioSource Char1AudioSource, Char2AudioSource;
 
     public float waitTime;
 
@@ -47,34 +108,276 @@ public class LineManager : MonoBehaviour
         allLinesIDList = lineID.ToList();
     }
 
-
     public void SortLineIDs()
     {
         foreach (string tempString in allLinesIDList)
         {
-            if (tempString.Contains("line:char1"))
+            //if (tempString.Contains("char1"))
+            //{
+            //    char1LineIDList.Add(tempString);
+            //}
+            //if (tempString.Contains("char2"))
+            //{
+            //    char2LineIDList.Add(tempString);
+            //}
+            //if (tempString.Contains("char1Negative"))
+            //{
+            //    char1NegativeLineIDList.Add(tempString);
+            //}
+            //if (tempString.Contains("char2Negative"))
+            //{
+            //    char2NegativeLineIDList.Add(tempString);
+            //}
+            //if (tempString.Contains("char1Neutral"))
+            //{
+            //    char1NeutralLineIDList.Add(tempString);
+            //}
+            //if (tempString.Contains("char2Neutral"))
+            //{
+            //    char2NeutralLineIDList.Add(tempString);
+            //}
+            //if (tempString.Contains("char1Positive"))
+            //{
+            //    char1PositiveLineIDList.Add(tempString);
+            //}
+            //if (tempString.Contains("char2Positive"))
+            //{
+            //    char2PositiveLineIDList.Add(tempString);
+            //}
+
+            if (tempString.Contains("char1NegativeStage1"))
             {
-                char1LineIDList.Add(tempString);
+                char1NegativeLineStage1IDList.Add(tempString);
             }
-            if (tempString.Contains("line:char2"))
+            if (tempString.Contains("char2NegativeStage1"))
             {
-                char2LineIDList.Add(tempString);
+                char2NegativeLineStage1IDList.Add(tempString);
+            }
+            if (tempString.Contains("char1NeutralStage1"))
+            {
+                char1NeutralLineStage1IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2NeutralStage1"))
+            {
+                char2NeutralLineStage1IDList.Add(tempString);
+            }
+            if (tempString.Contains("char1PositiveStage1"))
+            {
+                char1PositiveLineStage1IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2PositiveStage1"))
+            {
+                char2PositiveLineStage1IDList.Add(tempString);
+            }
+
+            if (tempString.Contains("char1NegativeStage2"))
+            {
+                char1NegativeLineStage2IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2NegativeStage2"))
+            {
+                char2NegativeLineStage2IDList.Add(tempString);
+            }
+            if (tempString.Contains("char1NeutralStage2"))
+            {
+                char1NeutralLineStage2IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2NeutralStage2"))
+            {
+                char2NeutralLineStage2IDList.Add(tempString);
+            }
+            if (tempString.Contains("char1PositiveStage2"))
+            {
+                char1PositiveLineStage2IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2PositiveStage2"))
+            {
+                char2PositiveLineStage2IDList.Add(tempString);
+            }
+
+            if (tempString.Contains("char1NegativeStage3"))
+            {
+                char1NegativeLineStage3IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2NegativeStage3"))
+            {
+                char2NegativeLineStage3IDList.Add(tempString);
+            }
+            if (tempString.Contains("char1NeutralStage3"))
+            {
+                char1NeutralLineStage3IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2NeutralStage3"))
+            {
+                char2NeutralLineStage3IDList.Add(tempString);
+            }
+            if (tempString.Contains("char1PositiveStage3"))
+            {
+                char1PositiveLineStage3IDList.Add(tempString);
+            }
+            if (tempString.Contains("char2PositiveStage3"))
+            {
+                char2PositiveLineStage3IDList.Add(tempString);
             }
         }
     }
 
     public void GetLinesFromIDs()
     {
-        foreach (string tempString in char1LineIDList)
+        //foreach (string tempString in char1LineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char1TextLineList.Add(text);
+        //}
+
+        //foreach (string tempString in char2LineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char2TextLineList.Add(text);
+        //}
+
+        //foreach (string tempString in char1NegativeLineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char1NegativeLineList.Add(text);
+        //}
+
+        //foreach (string tempString in char2NegativeLineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char2NegativeLineList.Add(text);
+        //}
+
+        //foreach (string tempString in char1NeutralLineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char1NeutralLineList.Add(text);
+        //}
+
+        //foreach (string tempString in char2NeutralLineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char2NeutralLineList.Add(text);
+        //}
+
+        //foreach (string tempString in char1PositiveLineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char1PositiveLineList.Add(text);
+        //}
+
+        //foreach (string tempString in char2PositiveLineIDList)
+        //{
+        //    var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+        //    char2PositiveLineList.Add(text);
+        //}
+
+        foreach (string tempString in char1NegativeLineStage1IDList)
         {
             var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
-            char1TextLineList.Add(text);
+            char1NegativeLineStage1List.Add(text);
         }
 
-        foreach (string tempString in char2LineIDList)
+        foreach (string tempString in char2NegativeLineStage1IDList)
         {
             var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
-            char2TextLineList.Add(text);
+            char2NegativeLineStage1List.Add(text);
+        }
+
+        foreach (string tempString in char1NeutralLineStage1IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1NeutralLineStage1List.Add(text);
+        }
+
+        foreach (string tempString in char2NeutralLineStage1IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2NeutralLineStage1List.Add(text);
+        }
+
+        foreach (string tempString in char1PositiveLineStage1IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1PositiveLineStage1List.Add(text);
+        }
+
+        foreach (string tempString in char2PositiveLineStage1IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2PositiveLineStage1List.Add(text);
+        }
+
+        foreach (string tempString in char1NegativeLineStage2IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1NegativeLineStage2List.Add(text);
+        }
+
+        foreach (string tempString in char2NegativeLineStage2IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2NegativeLineStage2List.Add(text);
+        }
+
+        foreach (string tempString in char1NeutralLineStage2IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1NeutralLineStage2List.Add(text);
+        }
+
+        foreach (string tempString in char2NeutralLineStage2IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2NeutralLineStage2List.Add(text);
+        }
+
+        foreach (string tempString in char1PositiveLineStage2IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1PositiveLineStage2List.Add(text);
+        }
+
+        foreach (string tempString in char2PositiveLineStage3IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2PositiveLineStage3List.Add(text);
+        }
+
+        foreach (string tempString in char1NegativeLineStage3IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1NegativeLineStage3List.Add(text);
+        }
+
+        foreach (string tempString in char2NegativeLineStage3IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2NegativeLineStage3List.Add(text);
+        }
+
+        foreach (string tempString in char1NeutralLineStage3IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1NeutralLineStage3List.Add(text);
+        }
+
+        foreach (string tempString in char2NeutralLineStage3IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2NeutralLineStage3List.Add(text);
+        }
+
+        foreach (string tempString in char1PositiveLineStage3IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char1PositiveLineStage3List.Add(text);
+        }
+
+        foreach (string tempString in char2PositiveLineStage3IDList)
+        {
+            var text = myYarnProject.GetLocalization("En").GetLocalizedString(tempString);
+            char2PositiveLineStage3List.Add(text);
         }
 
         //var text = myYarnProject.GetLocalization("En").GetLocalizedString("line:01c1039");
